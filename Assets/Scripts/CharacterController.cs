3238748e572b9daa96deeec6f1ce7ae42be9b901
EgorBykadarov <Demonik4053@gmail.com>
2024-04-04 15:54:48 +0700

    Initial commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public event Action OnGrounding;
+
+    private const float GroundedRadius = .2f;
+
+    [SerializeField] private float _jumpForce = 400f;
+    [SerializeField, Range(0, .3f)] private float _movementSmoothing = .05f;
+    [SerializeField] private Transform _groundCheck;
+
+    private Rigidbody2D _rigitbody2D;
+
+    private bool _grounded;
+    private bool _facingLeft = true;
+    private Vector3 _velocity = Vector3.zero;
+
+    private void Awake()
+    {
+        _rigitbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        bool wasGrounded = _grounded;
+        _grounded = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_groundCheck.position, GroundedRadius);
+
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                _grounded = true;
+
+                if(wasGrounded == false)
+                    OnGrounding?.Invoke();
+            }
+        }
+    }
+
+    public void Move(float move, bool jump)
+    {
+        Vector3 targetVelocity = new Vector2(move, _rigitbody2D.velocity.y);
+        _rigitbody2D.velocity = Vector3.SmoothDamp(_rigitbody2D.velocity, targetVelocity, ref _velocity, _movementSmoothing);
+
+        if (move > 0 && _facingLeft == true)
+            Flip();
+        else if (move < 0 && _facingLeft == false)
+            Flip();
+
+        if(_grounded && jump)
+        {
+            _grounded = false;
+            _rigitbody2D.AddForce(new Vector2(0, _jumpForce));
+        }
+    }
+
+    private void Flip()
+    {
+        _facingLeft = !_facingLeft;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private const string Horizontal = nameof(Horizontal);
+    private const string Jump = nameof(Jump);
+
+    [SerializeField, Range(100, 500)] private float _speed = 300f;
+
+    private CharacterController _controller;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw(Horizontal) * _speed;
+
+        if(Input.GetButtonDown(Jump))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
