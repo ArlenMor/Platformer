0583e17896502ca244c304a5ad5b9a554471539e
Arlen <Demonik4053@gmail.com>
2024-11-21 00:32:05 +0700

    Add EnemyPatrollerAttack

---
+++ Assets/Scripts/Enemy/EnemyPatrollerAttack.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrollerAttack : MonoBehaviour
+{
+    [SerializeField, Range(1,10)] private float _attackRange;
+
+    private RaycastHit2D _hit;
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    private void FixedUpdate()
+    {
+        _hit = Physics2D.Raycast(transform.position, transform.forward * _attackRange);
+
+        if(_hit.collider != null)
+        {
+
+        }
+    }
+
+    
+}
